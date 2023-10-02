9ecac993ad666a7587d16fbb97e5a0758e4d255b
soywiz <soywiz@gmail.com>
2023-10-02 17:59:47 +0200

    Bump KorGE to 5.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,14 @@
 import korlibs.korge.gradle.*
 
 plugins {
-    //alias(libs.plugins.korge)
-    id("com.soywiz.korge") version "4.0.3"
+    alias(libs.plugins.korge)
 }
 
 korge {
     id = "org.korge.samples.demoldtk"
 
-// To enable all targets at once
-
-    //targetAll()
-
-// To enable targets based on properties/environment variables
-    //targetDefault()
-
-// To selectively enable targets
-
     targetJvm()
     targetJs()
-    targetDesktop()
-    //targetDesktopCross()
     targetIos()
     targetAndroid()
 
