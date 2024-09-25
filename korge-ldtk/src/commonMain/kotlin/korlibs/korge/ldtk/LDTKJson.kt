f2ba0a2a40fae71e3531833d3df3a7ad0a6cc9c5
Marko Koschak <marko.koschak@tisno.de>
2024-09-25 13:00:59 +0200

    Fix loading tileRectsIds in AutoLayerRuleGroup - WIP

--- korge-ldtk/src/commonMain/kotlin/korlibs/korge/ldtk/LDTKJson.kt
+++ korge-ldtk/src/commonMain/kotlin/korlibs/korge/ldtk/LDTKJson.kt
@@ -862,7 +862,7 @@ data class LayerDefinition (
     /**
      * Contains all the auto-layer rule definitions.
      */
-    val autoRuleGroups: List<AutoLayerRuleGroup>,
+//    val autoRuleGroups: List<AutoLayerRuleGroup>,
 
     val autoSourceLayerDefUid: Int? = null,
 
@@ -1072,7 +1072,7 @@ data class AutoLayerRuleDefinition (
     /**
      * Array of all the tile IDs. They are used randomly or as stamps, based on `tileMode` value.
      */
-    @SerialName("tileIds")
+    @SerialName("tileRectsIds")
     val tileIDS: IntArray,
 
     /**
